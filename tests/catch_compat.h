c5bf045202e50db466a9536472801ad1e7c394c7
Mark de Bruijn <mrdebruijn@gmail.com>
2026-03-01 19:24:24 +0100

    ci: enforce linux test execution and support catch2 v2/v3

---
+++ tests/catch_compat.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#if defined(__has_include)
+#if __has_include(<catch2/catch_test_macros.hpp>)
+#include <catch2/catch_test_macros.hpp>
+#elif __has_include(<catch2/catch.hpp>)
+#include <catch2/catch.hpp>
+#else
+#error "Catch2 headers not found"
+#endif
+#else
+#include <catch2/catch_test_macros.hpp>
+#endif
